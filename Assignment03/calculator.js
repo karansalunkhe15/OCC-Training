e9d284a669e360ceba6c0155d803aebf799122e5
karansalunkhe15 <89115640+karansalunkhe15@users.noreply.github.com>
2021-11-16 09:53:09 +0530

    Add files via upload

---
+++ Assignment03/calculator.js
@@ -0,0 +1,21 @@
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function sub(n1, n2) {
+  return n1 - n2;
+}
+
+function div(n1, n2) {
+  return n1 / n2;
+}
+
+function mult(n1, n2) {
+  return n1 * n2;
+}
+
+module.exports.add = add;
+
+module.exports.sub = sub;
+module.exports.div = div;
+module.exports.mult = mult;
---
+++ Assignment03/index.js
@@ -0,0 +1,11 @@
+//calculator functionality
+
+const calculator = require("./calculator");
+
+console.log("add " + calculator.add(10, 20));
+
+console.log("sub " + calculator.sub(10, 20));
+
+console.log("div " + calculator.div(10, 20));
+
+console.log("mult " + calculator.mult(10, 20));
