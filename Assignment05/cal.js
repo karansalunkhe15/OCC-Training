76372887c3fca014669f1a580448294c357de424
karansalunkhe15 <89115640+karansalunkhe15@users.noreply.github.com>
2021-11-16 09:53:42 +0530

    Add files via upload

---
+++ Assignment05/cal.js
@@ -0,0 +1,17 @@
+//Assignment 5:
+// Create Calculator using Clousure
+const cal = (function () {
+  let num = 10;
+  return function () {
+    add = num += 1;
+    sub = num -= 2;
+    mul = num *= 1;
+    div = num /= 2;
+    console.log("addition: " + add);
+    console.log("Subtraction: " + sub);
+    console.log("Multiplication: " + mul);
+    console.log("Division: " + div);
+  };
+})();
+
+cal();
