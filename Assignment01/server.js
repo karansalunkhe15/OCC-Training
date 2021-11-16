eeed4627c5cf12bcc6533a19221ff6ba5ebb982b
karansalunkhe15 <89115640+karansalunkhe15@users.noreply.github.com>
2021-11-16 09:50:06 +0530

    Add files via upload

---
+++ Assignment01/server.js
@@ -0,0 +1,14 @@
+//create server
+const http = require("http");
+
+const port = 3000;
+
+http
+  .createServer(function (req, resp) {
+    resp.write("<h1>Good Morning </h1>");
+
+    resp.end();
+  })
+  .listen(port, function () {
+    console.log(`Custom server is working at : http://localhost:${port}`);
+  });
